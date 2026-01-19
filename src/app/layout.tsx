dd01f5a3b14a75b6bf9dd9c029ae480c5c8ba3a7
Abdelaziz Ibrahim <abdelaziz_elshrkawy53@yahoo.com>
2026-01-19 11:34:37 +0200

    Revert "adding shortcut icon for google searhc"
    This reverts commit 5176ff33296bc0d1dab43d13f3be15aa9bc7a79b.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,6 @@ export const metadata: Metadata = {
   verification: {
     google: "tyka86vTYgbEMPDGsiKXSwQttHIWGfgalb_y5TX0Fbc",
   },
-  icons: {
-    icon: "favicon.ico",
-    shortcut: "favicon.ico",
-  },
 };
 
 export default function RootLayout({
